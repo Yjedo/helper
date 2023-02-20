94dfb22e11cf98c16ca8f149388d168c72877c25
Yjedo <2050333463@qq.com>
2023-02-20 14:17:28 +0800

    完善子模块包结构

--- helper-back-end/user/src/main/java/com/yyd/helper/user/UserApplication.java
+++ helper-back-end/user/src/main/java/com/yyd/helper/user/UserApplication.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
     @GetMapping("getUserById")
